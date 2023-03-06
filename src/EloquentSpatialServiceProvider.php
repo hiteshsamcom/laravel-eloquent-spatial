56c67a0c298e61cd189f5c2ac93c0127ee5fcf31
Matan Yadaev <matan@zerolimits.io>
2023-03-06 23:45:35 +0200

    Make doctrine types configurable

--- src/EloquentSpatialServiceProvider.php
+++ src/EloquentSpatialServiceProvider.php
@@ -4,21 +4,19 @@ declare(strict_types=1);
 
 namespace MatanYadaev\EloquentSpatial;
 
+use Doctrine\DBAL\Types\Type;
 use Illuminate\Database\Connection;
 use Illuminate\Database\DatabaseServiceProvider;
 use Illuminate\Support\Facades\DB;
-use MatanYadaev\EloquentSpatial\Doctrine\GeometryCollectionType;
-use MatanYadaev\EloquentSpatial\Doctrine\LineStringType;
-use MatanYadaev\EloquentSpatial\Doctrine\MultiLineStringType;
-use MatanYadaev\EloquentSpatial\Doctrine\MultiPointType;
-use MatanYadaev\EloquentSpatial\Doctrine\MultiPolygonType;
-use MatanYadaev\EloquentSpatial\Doctrine\PointType;
-use MatanYadaev\EloquentSpatial\Doctrine\PolygonType;
 
 class EloquentSpatialServiceProvider extends DatabaseServiceProvider
 {
   public function boot(): void
   {
+    $this->publishes([
+      __DIR__.'/../config/eloquent-spatial.php' => config_path('eloquent-spatial.php'),
+    ]);
+
     /** @var Connection $connection */
     $connection = DB::connection();
 
@@ -27,20 +25,21 @@ class EloquentSpatialServiceProvider extends DatabaseServiceProvider
     }
   }
 
+  public function register()
+  {
+    $this->mergeConfigFrom(
+      __DIR__.'/../config/eloquent-spatial.php',
+      'eloquent-spatial'
+    );
+  }
+
   protected function registerDoctrineTypes(Connection $connection): void
   {
-    $geometries = [
-      'point' => PointType::class,
-      'linestring' => LineStringType::class,
-      'multipoint' => MultiPointType::class,
-      'polygon' => PolygonType::class,
-      'multilinestring' => MultiLineStringType::class,
-      'multipolygon' => MultiPolygonType::class,
-      'geometrycollection' => GeometryCollectionType::class,
-      'geomcollection' => GeometryCollectionType::class,
-    ];
+    # @TODO assert this
+    /** @var array<string, class-string<Type>> $doctrineTypes */
+    $doctrineTypes = config()->get('eloquent-spatial.doctrine_types');
 
-    foreach ($geometries as $type => $class) {
+    foreach ($doctrineTypes as $type => $class) {
       DB::registerDoctrineType($class, $type, $type);
       $connection->registerDoctrineType($class, $type, $type);
     }
